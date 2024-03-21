abd66176162c1883880d0f8df898c65f0113029a
Hehs33 <enriquehidrobo15@gmail.com>
2024-03-20 21:03:13 -0500

    Vista desde home

--- core/urls.py
+++ core/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from applications.home.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index, name = 'index'),
+    path('home/', IndexView.as_view(), name = 'home'),
 ]
