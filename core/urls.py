57073ee834e3c41784e3c8f88933e2471d111211
Hehs33 <enriquehidrobo15@gmail.com>
2024-03-25 23:52:18 -0500

    Creacion de templates home

--- core/urls.py
+++ core/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
-from applications.home.views import IndexView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index, name = 'index'),
-    path('home/', IndexView.as_view(), name = 'home'),
+    path('', include('applications.home.urls')),
 ]
